9b78a370770027f518ce2c5cb595c416318b1b37
SpielerNogard <christophermueller2@outlook.de>
2022-03-28 12:23:33 +0200

    logging

--- webhook.py
+++ webhook.py
@@ -1,18 +1,19 @@
 from flask import Flask,request, abort
 import json
 import paho.mqtt.client as mqtt
+from logging import getLogger
 TOPIC3 = "Pokefinder/answers"
 BROKER_ADDRESS = "0.0.0.0"
 PORT = 1883
 QOS = 1
 app = Flask(__name__)
-
+logger = getLogger(__name__)
 
 @app.route("/", methods=['POST'])
 def index():
     try:
         data = json.loads(request.data)
-        print(f'Received Webhook from {request.remote_addr}')
+        logger.info(f'Received Webhook from {request.remote_addr}')
         if isinstance(data, list):
             for a in data:
                 client = mqtt.Client()
@@ -20,17 +21,17 @@ def index():
                 
                 client.publish(f"MAD/{a.get('type', 'fail')}", json.dumps(a), qos=QOS)
                 client.loop()
-                print(f"Published on: MAD/{a.get('type', 'fail')}")
+                logger.info(f"Published on: MAD/{a.get('type', 'fail')}")
         else:
             client = mqtt.Client()
             client.connect(BROKER_ADDRESS, PORT)
-            print("Connected to MQTT Broker: " + BROKER_ADDRESS)
+            logger.info("Connected to MQTT Broker: " + BROKER_ADDRESS)
             client.publish(f"MAD/{data.get('type', 'fail')}", json.dumps(data), qos=QOS)
             client.loop()
-            print(f"Published on: MAD/{data.get('type', 'fail')}")
+            logger.info(f"Published on: MAD/{data.get('type', 'fail')}")
         return "OK"
     except Exception as e:
-        print(f'{e}')
+        logger.error(f'{e}')
         return abort(400)
 
 
